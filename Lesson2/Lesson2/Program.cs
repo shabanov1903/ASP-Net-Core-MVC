dd4887311cd3e6b20ab8448a3e99ee2c91f65ce8
Danil Shabanov <shabanov1903@gmail.com>
2022-04-03 13:50:22 +0300

    lesson2
    Dive into multithreading

---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,14 @@
+﻿using Lesson2;
+
+try
+{
+    var f = new SimpleThreadPool(5);
+    for (int i = 0; i < 50; i++) {
+        var arg = i;
+        f.AddTask(() => Console.WriteLine(arg));
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
