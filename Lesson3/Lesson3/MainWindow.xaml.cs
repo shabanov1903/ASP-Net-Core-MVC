a7dc320b403f94a2fff5bf3d00cca64b97dc70b3
Danil Shabanov <shabanov1903@gmail.com>
2022-04-03 17:58:06 +0300

    lesson3
    Introduction to patterns. Generative

---
+++ Lesson3/Lesson3/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Lesson3.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SimpleRGB rgb;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            rgb = SimpleRGB.Create()
+                           .AddRedColorControl(LevelRed)
+                           .AddGreenColorControl(LevelGreen)
+                           .AddBlueColorControl(LevelBlue)
+                           .Build();
+
+            rgb.ColorChanged += Rgb_ColorChanged;
+        }
+
+        private void Rgb_ColorChanged(object? sender, SolidColorBrush e)
+        {
+            ShowWindow.Background = e;
+        }
+
+        private void ButtonCalc_Click(object sender, RoutedEventArgs e)
+        {
+            rgb.Calculate();
+        }
+    }
+}
---
+++ Lesson3/Lesson3/Services/ICommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3.Services
+{
+    public interface ICommand<T>
+    {
+        /// <summary>
+        /// При реализации интерфейса событие должно быть вызвано в конце метода Calculate()
+        /// В событии передается информация после выполнения метода Calculate()
+        /// </summary>
+        public event EventHandler<T> ColorChanged;
+
+        /// <summary>
+        /// Метод вызывается извне для расчета параметров заранее сконфигурированного объекта
+        /// Информация передается в событии для возможности использования данных не только в месте вызова
+        /// </summary>
+        public void Calculate();
+    }
+}
