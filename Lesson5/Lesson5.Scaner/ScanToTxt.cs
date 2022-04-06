c1255e4d2f4b3ff366477fbce0fc2124565862bd
Danil Shabanov <shabanov1903@gmail.com>
2022-04-06 22:12:39 +0300

    lesson5
    Introduction to patterns. Behavioral

---
+++ Lesson5/Lesson5.Scaner/ScanToTxt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5.Scaner
+{
+    public class ScanToTxt : ISaver
+    {
+        IScanning _Scanner;
+        public ScanToTxt(IScanning scanner) => _Scanner = scanner;
+        public void Save(string file, string path)
+        {
+            string ex = ".txt";
+            string str = _Scanner.GetData(file);
+
+            var scanNum = 0;
+            while (File.Exists(path + @"\scan" + scanNum + ex))
+            {
+                scanNum++;
+            }
+
+            using (StreamWriter sw = File.CreateText(path + @"\scan" + scanNum + ex))
+            {
+                if (str != String.Empty) sw.WriteAsync(str);
+            }
+        }
+    }
+}
