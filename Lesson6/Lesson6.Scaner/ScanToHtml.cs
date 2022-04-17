698dc37fc4f8d3624fc6af6a46cb21a4405f3e7f
Danil Shabanov <shabanov1903@gmail.com>
2022-04-17 15:43:45 +0300

    Lesson6
    Autofac

---
+++ Lesson6/Lesson6.Scaner/ScanToHtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6.Scaner
+{
+    public class ScanToHtml : ISaver
+    {
+        IScanning _Scanner;
+        public ScanToHtml(IScanning scanner) => _Scanner = scanner;
+        public void Save(string file, string path)
+        {
+            string ex = ".html";
+            string str = _Scanner.GetData(file);
+
+            var scanNum = 0;
+            while (File.Exists(path + @"\scan" + scanNum + ex))
+            {
+                scanNum++;
+            }
+            
+            using (StreamWriter sw = File.CreateText(path + @"\scan" + scanNum + ex))
+            {
+                if (str != String.Empty) sw.WriteAsync(str);
+            }
+        }
+    }
+}
---
+++ Lesson6/Lesson6/Program.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using Lesson6.Scaner;
+
+string file = @"D:\Programming\Testing\test.txt";
+string path = @"D:\Programming\Testing\TestDirectory";
+
+var builder = new ContainerBuilder();
+builder.RegisterType<Scanner>().As<IScanning>();
+builder.RegisterType<ScanToTxt>().Keyed<ISaver>("txt");
+builder.RegisterType<ScanToHtml>().Keyed<ISaver>("html");
+builder.RegisterType<ScanToRtf>().Keyed<ISaver>("rtf");
+
+IContainer container = builder.Build();
+
+var saver = container.ResolveKeyed<ISaver>("rtf");
+
+var scanner = new ScannerManager(saver);
+scanner.StartScan(file, path);
