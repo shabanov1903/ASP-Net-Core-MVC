292324252347589f26612da903c501b57710fd6c
Danil Shabanov <dvshabanov@baltbet.ru>
2022-04-20 13:56:24 +0300

    Lesson7
    Template engines

---
+++ Lesson7/Lesson7.Reporter/ReporterManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7.Reporter
+{
+    public class ReporterManager
+    {
+        public static Queue<IReport> _reporters = new Queue<IReport>();
+        private string _path;
+
+        public ReporterManager(string path)
+        {
+            _path = path;
+        }
+
+        public ReporterManager AddPart(IReport report)
+        {
+            _reporters.Enqueue(report);
+            return this;
+        }
+
+        private void Next(IReport reporter)
+        {
+            reporter.Generate(_path);
+            if (_reporters.Count > 0)
+            {
+                Next(_reporters.Dequeue());
+            }
+        }
+
+        public void Start()
+        {
+            if (_reporters.Count > 0)
+            {
+                Next(_reporters.Dequeue());
+            }
+        }
+    }
+}
